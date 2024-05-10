d4acbebf5103909e3a909788c7676cda1bda93ef
Hector Napier <HectorNapier@gmail.com>
2024-05-09 23:12:41 -0500

    Done for grading

--- obj/Debug/net8.0/CIDM-3312-Final.AssemblyInfo.cs
+++ obj/Debug/net8.0/CIDM-3312-Final.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CIDM-3312-Final")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c300cc1198807c1b78a3d2bc2dfcccee422994")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a797904aef0da864b2a15e588e1823e65840b0fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("CIDM-3312-Final")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CIDM-3312-Final")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
